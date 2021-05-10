8045a9699236d7ec649041dde5fb3808aeb209af
Flavia Lima <mbl.flavia@gmail.com>
2021-05-10 00:44:21 -0300

    Aula 07 V.O.

--- 05 -  REST_ADID6 Scaffold V.O/src/main/java/br/edu/ifsp/controller/PersonController.java
+++ 05 -  REST_ADID6 Scaffold V.O/src/main/java/br/edu/ifsp/controller/PersonController.java
@@ -3,6 +3,7 @@ package br.edu.ifsp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.edu.ifsp.model.Person;
+import br.edu.ifsp.data.vo.PersonVO;
 import br.edu.ifsp.services.PersonServices;
-
 
 @RestController
 @RequestMapping("/person")
@@ -24,27 +24,27 @@ public class PersonController {
 	private PersonServices services;
 	
 	@GetMapping("/{id}")
-	public Person findById(@PathVariable("id") Long id) {
+	public PersonVO findById(@PathVariable("id") Long id) {
 		return services.findById(id);
 	}
 	
 	@PostMapping
-	public Person create(@RequestBody Person person) {
+	public PersonVO create(@RequestBody PersonVO person) {
 		return services.create(person);
 	}
 	
 	@PutMapping
-	public Person update(@RequestBody Person person) {
+	public PersonVO update(@RequestBody PersonVO person) {
 		return services.update(person);
 	}
 	
 	@DeleteMapping("/{id}")
-	public void delete(@PathVariable("id") Long id) {
-		services.delete(id);
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+		return ResponseEntity.ok().build();
 	}
 	
 	@GetMapping
-	public List<Person> findAll() {
+	public List<PersonVO> findAll() {
 		return services.findAll();
 	}
 }
