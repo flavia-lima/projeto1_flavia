2adc39de1ab1dbfe0d403f50dc87e38dc6de3daf
Flavia Lima <mbl.flavia@gmail.com>
2021-06-28 22:29:10 -0300

    Aula 11 - HATEOAS corrigir.

---
+++ 09 - REST_ADID6 Scaffold HATEOAS/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Av Paulista', 'Flávia', 'Female', 'Lima'),
+	(3, 'Rua dos Alfeneiros, 4', 'Harry', 'Male', 'Potter'),
+	(4, 'Floresta Proibida', 'Lord', 'Male', 'Voldemort'),
+	(5, 'Fenda do Biquini', 'Bob', 'Male', 'Esponja'),
+	(6, 'Fenda do Biquini', 'Bob', 'Male', 'Esponja Calca Quadrada'),
+	(7, 'Fenda do Biquini', 'Bob', 'Male', 'Esponja Calca Quadrada');
